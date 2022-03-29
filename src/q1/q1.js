c35484e53b3ade637bf50539c517a815bba3f5ae
ruru <coco002197@gmail.com>
2022-03-29 09:44:47 +0800

    refactor(leetcode-q1): refactor detail of JSDoc

--- src/q1/q1.js
+++ src/q1/q1.js
@@ -11,29 +11,28 @@
  */
 
 /**
- * return index of element in array that equals target
- * @param {Array} numbers
- * @param {Number} target
- * @return {Array}
+ * Returns the index of element in array that equals target
+ * @param {number []} numbers - A list of numbers
+ * @param {number} target - Sum of two numbers
+ * @returns {number []}  Index of two numbers that add up to target
  */
 function twoSum(numbers, target) {
     if (!Array.isArray(numbers)) throw new Error()
     if (typeof target !== 'number') throw new Error()
 
-    // 解法：1 一個一個迴圈比較直到相加相同
-    // 解法二：2 先排序，再比較
     // 2.1 先按照大小排序
     const sortedNumbers = numbers.slice().sort((a, b) => a - b)
     let minIndex = 0
     let maxIndex = numbers.length - 1
-    // 2.2 再來將最小和最大數字相加，如果相加等於target，則回傳
-    // 2.3 如果相加不等於target，若是大於 target 則最大值向左一格；反之則小於 target 則最小值向右一格
+
     while (minIndex < maxIndex) {
+        // 2.3 如果相加不等於target，若是大於 target 則最大值向左一格；反之則小於 target 則最小值向右一格
         if (sortedNumbers[minIndex] + sortedNumbers[maxIndex] > target) {
             maxIndex--
         } else if (sortedNumbers[minIndex] + sortedNumbers[maxIndex] < target) {
             minIndex++
         } else {
+            // 2.2 再來將最小和最大數字相加，如果相加等於target，則回傳
             return [
                 numbers.indexOf(sortedNumbers[minIndex]),
                 numbers.lastIndexOf(sortedNumbers[maxIndex]),
