9efcd042b7a240cdf1fd42baaa47490b6c0ff1ce
ruru <coco002197@gmail.com>
2022-03-30 09:45:41 +0800

    doc: add note to leetcode 3

--- src/q2/q2.js
+++ src/q2/q2.js
@@ -1,30 +1,30 @@
 /**
  * Return length of longest substring of s without repeating characters
- * @param {string} s
+ * @param {string} string
  * @return {number} length of longest substring without repeating characters
  */
-function lengthOfLongestSubstring(s) {
-    if (typeof s !== 'string') throw new Error('s must be a string')
-    if (s.length === 0) return 0
+function lengthOfLongestSubstring(string) {
+    if (typeof string !== 'string') throw new Error('s must be a string')
+    if (string.length === 0) return 0
+
+    // use two pointers
     let left = 0
     let right = 0
     let result = ''
-    let set = new Set()
+    let substringSet = new Set()
 
-    while (right < s.length) {
-        const substring = s.substring(left, right + 1)
-        const reStart = false
-        if (set.has(s.substring(right, right + 1))) {
-            left ++
-        } else {
-            right++
-            result = result.length < substring.length ? substring : result
-        }
-        set.clear()
-        set = new Set(s.substring(left, right).split(''))
+    while (right < string.length) {
+        const substring = string.substring(left, right + 1)
+        const currentChar = string.substring(right, right + 1)
+
+        if (substringSet.has(currentChar)) left++
+        else right++
+
+        substringSet = new Set(string.substring(left, right).split(''))
+        result = result.length < substring.length ? substring : result
     }
 
     return result.length
 }
-// lengthOfLongestSubstring('aa')
+
 export default lengthOfLongestSubstring
