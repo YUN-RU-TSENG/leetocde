36b72bc0d2992a4304b1cc7a9b08aa88064b71cb
ruru <coco002197@gmail.com>
2022-04-03 01:51:45 +0800

    feat(leetcode-5): ver.2

--- src/q3/q3.js
+++ src/q3/q3.js
@@ -1,61 +1,30 @@
-// function longestPalindrome(string) {
-//     if (typeof string !== 'string') throw new Error('string is not correct type')
-//     if (!string.length) return string
-
-//     let result = ''
-
-//     for (let i = 0; i < string.length; i++) {
-//         for (let j = i; j < string.length; j++) {
-//             const currentSubstring = string.slice(i, j + 1)
-//             const reverseCurrentSubstring = currentSubstring.split('').reverse().join('')
-//             if (currentSubstring === reverseCurrentSubstring) {
-//                 if (currentSubstring.length > result.length) {
-//                     result = currentSubstring
-//                 }
-//             }
-//             continue;
-//         }
-//     }
-//     return result
-// }
-
 function longestPalindrome(string) {
     if (typeof string !== 'string') throw new Error('string is not correct type')
     if (string.length < 2) return string
 
     let result = ''
-    let a = 0
-    let b = 0
+    const array2D = string.split('').map((index) => new Array(index).fill(null))
 
-    a: for (let i = 0; i < string.length; i++) {
-        for (let j = 0; j <= i; j++) {
-            a++
-            const currentSubstring = string.slice(i - j, i + j + 1)
-            const reverseCurrentSubstring = currentSubstring.split('').reverse().join('')
-            if (currentSubstring !== reverseCurrentSubstring) continue a
-            if (currentSubstring.length > result.length) {
-                result = currentSubstring
+    for (let i = 0; i < array2D.length; i++) {
+        array2D[i][i] = true
+        if (result.length < string.slice(i, i + 1).length) result = string.slice(i, i + 1)
+    }
+
+    for (let i = 0; i < array2D.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (Math.abs(i - j) <= 1) {
+                array2D[i][j] = string[i] === string[j]
+                if (array2D[i][j] && result.length < string.slice(j, i + 1).length)
+                    result = string.slice(j, i + 1)
+            } else {
+                array2D[i][j] = !!(string[i] === string[j] && array2D[i - 1][j + 1])
+                if (array2D[i][j] && result.length < string.slice(j, i  + 1).length)
+                    result = string.slice(j, i + 1)
             }
         }
     }
 
-    const secondString = string.split('').reverse().join('$')
-    b: for (let i = 0; i < secondString.length; i++) {
-        for (let j = 0; j <= i; j++) {
-            b++
-            const currentSubstring = secondString.slice(i - j, i + j + 1)
-            const reverseCurrentSubstring = currentSubstring.split('').reverse().join('')
-            if (currentSubstring !== reverseCurrentSubstring) continue b
-            const currentSubstringCorrect = currentSubstring.split('$').reverse().join('')
-            if (currentSubstringCorrect.length > result.length) {
-                result = currentSubstringCorrect
-            }
-        }
-    }
-
-    console.log(a, b)
     return result
 }
 
-// longestPalindrome('abba')
 export default longestPalindrome
