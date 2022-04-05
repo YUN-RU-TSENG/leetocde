184ad8669745f8c16177481e0e6f0f805ede2d73
ruru <coco002197@gmail.com>
2022-04-05 15:47:02 +0800

    refactor(leetcode q4): ver.2

--- src/q4/q4.js
+++ src/q4/q4.js
@@ -3,27 +3,22 @@
  * @param {number[]} height - heights of each bar
  * @return {number} max area
  */
-function maxArea(height) {
+function getMaxArea(height) {
     if (!Array.isArray(height)) throw new Error('height is not correct type')
     if (!height.length) return 0
 
-    let result = 0
+    let result = 0,
+        right = height.length - 1,
+        left = 0
 
-    for (let i = 0; i < height.length; i++) {
-        for (let j = height.length - 1; j >= 0; j--) {
-            const area = Math.min(height[i], height[j]) * (j - i)
-            if (area > result) result = area
-        }
+    while (left < right) {
+        result = Math.max(Math.min(height[left], height[right]) * (right - left), result)
+
+        if (height[left] < height[right]) left++
+        else right--
     }
 
     return result
 }
 
-export default maxArea
-// let left = 0, right = height.length - 1, max = 0;
-// while (left < right) {
-//     max = Math.max(max, Math.min(height[left], height[right]) * (right - left));
-//     if (height[left] < height[right]) left++;
-//     else right--;
-// }
-// return max;
+export default getMaxArea
