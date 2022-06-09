56bba7a1be5430571cbc9fff056877e26bdd244f
ruru <coco002197@gmail.com>
2022-06-09 22:23:04 +0800

    doc: q33

--- src/q33/index.js
+++ src/q33/index.js
@@ -3,10 +3,13 @@
  * @param {number} target
  * @return {number}
  */
-var search = function (nums, target) {
+function search(nums, target) {
     const last = nums[nums.length - 1]
+
     let answer = -1
 
+    // binary search
+    // 要達成 o(log n) ，由於是旋轉陣列，需要比較要找的數值比較接近頭部還是尾部，再從那個位置往下找
     if (target > last) {
         for (let i = 0; i < nums.length; i++) {
             if (nums[i] === target) answer = i
