09db4e7d8c58c777c6ccf757897a8bc8a61c6fde
ruru <coco002197@gmail.com>
2022-06-09 22:29:11 +0800

    refactor: 修正 binary search 比較要找的數值與中間差，再開始找

--- src/q33/index.js
+++ src/q33/index.js
@@ -4,13 +4,13 @@
  * @return {number}
  */
 function search(nums, target) {
-    const last = nums[nums.length - 1]
+    const center = nums[Math.floor(nums.length - 1)]
 
     let answer = -1
 
     // binary search
     // 要達成 o(log n) ，由於是旋轉陣列，需要比較要找的數值比較接近頭部還是尾部，再從那個位置往下找
-    if (target > last) {
+    if (target < center) {
         for (let i = 0; i < nums.length; i++) {
             if (nums[i] === target) answer = i
         }
